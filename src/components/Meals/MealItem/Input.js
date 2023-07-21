205fc8f3282438ba95589b21db2da8517f0bd868
Linh Nguyen <n.linh@oneglobal.com>
2023-07-21 22:46:21 +0700

    init project

---
+++ src/components/Meals/MealItem/Input.js
@@ -0,0 +1,9 @@
+import classes from "./Input.module.css"
+const Input = (props) => {
+    return <div className={classes.input}>
+        <label httmlFor={props.input.id}>{props.label}</label>
+        <input {...props.input}/>
+    </div>
+}
+
+export default Input;
